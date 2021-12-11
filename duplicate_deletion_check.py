364d24b373941e0b00555d26cded7c6fcaca77a6
Aaron Graham <Aaron@Aaron-Grahams-iMac.local>
2021-12-11 17:26:53 -0500

    Cleaned up code and output for duplicate finder and duplicate deletion check scripts

--- duplicate_deletion_check.py
+++ duplicate_deletion_check.py
@@ -1,50 +1,78 @@
 import os
 if __name__ == "__main__":
 
-	# Grab all movies in the backup photos library 
-	os.chdir("/Extra Backups/iMac System Photos Library Backup/Photos Library.photoslibrary")
-	os.system("find . -name '*.avi' > /Users/Aaron/Programming/Photo_Project/backup_movie_out.txt")
-	os.system("find . -name '*.AVI' >> /Users/Aaron/Programming/Photo_Project/backup_movie_out.txt")
-	os.system("find . -name '*.mov' >> /Users/Aaron/Programming/Photo_Project/backup_movie_out.txt")
-	os.system("find . -name '*.MOV' >> /Users/Aaron/Programming/Photo_Project/backup_movie_out.txt")
-	os.system("find . -name '*.mpg' >> /Users/Aaron/Programming/Photo_Project/backup_movie_out.txt")
-	os.chdir("/Users/Aaron/Programming/Photo_Project")
+	# Grab all movies in the backup photos library
+	BACKUP_PHOTO_LIB = "/Extra\ Backups/iMac\ System\ Photos\ Library\ Backup/Photos\ Library.photoslibrary"
+	os.system("find {} -name '*.avi' > backup_movie_out.txt".format(BACKUP_PHOTO_LIB))
+	os.system("find {} -name '*.AVI' >> backup_movie_out.txt".format(BACKUP_PHOTO_LIB))
+	os.system("find {} -name '*.mov' >> backup_movie_out.txt".format(BACKUP_PHOTO_LIB))
+	os.system("find {} -name '*.MOV' >> backup_movie_out.txt".format(BACKUP_PHOTO_LIB))
+	os.system("find {} -name '*.mpg' >> backup_movie_out.txt".format(BACKUP_PHOTO_LIB))
 	with open("backup_movie_out.txt", "r") as f:
 		backup_movies = f.readlines()
-	print(str(len(backup_movies)) + " total backup movies")
+	print(str(len(backup_movies)) + " total movie(s) in the backup photo library\n")
+
+	# Iterate through all movies in the backup photo library and get the name, time, and size of each movie
 	backup_movie_names = []
 	backup_movie_times = []
+	backup_movie_sizes = []
 	for i in range(len(backup_movies)):
-		movie = backup_movies[i][backup_movies[i].rfind("/", 0, backup_movies[i].rfind("/"))+1:len(backup_movies[i])-1]
-		backup_movie_names.append(movie[movie.find("/")+1:])
-		backup_movie_times.append(movie[0:movie.find("/")])
+		movie = backup_movies[i][:len(backup_movies[i])-1]
+		movie_cut = movie[movie.rfind("/", 0, movie.rfind("/"))+1:len(movie)]
+		backup_movie_names.append(movie_cut[movie_cut.find("/")+1:])
+		backup_movie_times.append(movie_cut[0:movie_cut.find("/")])
+		backup_movie_sizes.append(os.path.getsize(movie))
 
 	# Grab all movies in the current photos library
-	os.chdir("/Users/Aaron/Pictures/Photos Library.photoslibrary")
-	os.system("find . -name '*.avi' > /Users/Aaron/Programming/Photo_Project/current_movie_out.txt")
-	os.system("find . -name '*.AVI' >> /Users/Aaron/Programming/Photo_Project/current_movie_out.txt")
-	os.system("find . -name '*.mov' >> /Users/Aaron/Programming/Photo_Project/current_movie_out.txt")
-	os.system("find . -name '*.MOV' >> /Users/Aaron/Programming/Photo_Project/current_movie_out.txt")
-	os.system("find . -name '*.mpg' >> /Users/Aaron/Programming/Photo_Project/current_movie_out.txt")
-	os.chdir("/Users/Aaron/Programming/Photo_Project")
+	PHOTO_LIB = "/Users/Aaron/Pictures/Photos\ Library.photoslibrary"
+	os.system("find {} -name '*.avi' > current_movie_out.txt".format(PHOTO_LIB))
+	os.system("find {} -name '*.AVI' >> current_movie_out.txt".format(PHOTO_LIB))
+	os.system("find {} -name '*.mov' >> current_movie_out.txt".format(PHOTO_LIB))
+	os.system("find {} -name '*.MOV' >> current_movie_out.txt".format(PHOTO_LIB))
+	os.system("find {} -name '*.mpg' >> current_movie_out.txt".format(PHOTO_LIB))
 	with open("current_movie_out.txt", "r") as f:
 		current_movies = f.readlines()
-	print(str(len(current_movies)) + " total current movies")
+	print(str(len(current_movies)) + " total movie(s) in the current photo library\n")
+
+	# Iterate through all movies in the current photo library and get the name, time, and size of each movie
 	current_movie_names = []
 	current_movie_times = []
+	current_movie_sizes = []
 	for i in range(len(current_movies)):
-		movie = current_movies[i][current_movies[i].rfind("/", 0, current_movies[i].rfind("/"))+1:len(current_movies[i])-1]
-		current_movie_names.append(movie[movie.find("/")+1:])
+		movie = current_movies[i][:len(current_movies[i])-1]
+		movie_cut = movie[movie.rfind("/", 0, movie.rfind("/"))+1:len(movie)]
+		current_movie_names.append(movie_cut[movie_cut.find("/")+1:])
+		current_movie_times.append(movie_cut[0:movie_cut.find("/")])
+		current_movie_sizes.append(os.path.getsize(movie))
 
-	# If a movie is in the backup photos library and NOT in the current photos library, that means we have accidentally deleted a non-duplicate. Add it to the list.
-	bad_movie_names = []
-	bad_movie_times = []
-	for i in range(len(backup_movie_names)):
-		if backup_movie_names[i] not in current_movie_names:
-			bad_movie_names.append(backup_movie_names[i])
-			bad_movie_times.append(backup_movie_times[i])
+	# First, we want to search for missing movies based on missing movie sizes
+	# Then, we want to search for missing movies based on missing movie names
+	filter_variants = ['size', 'name']
+	for filter_variant in filter_variants:
+		if filter_variant == 'size':
+			backup_list = backup_movie_sizes
+			current_list = current_movie_sizes
+		elif filter_variant == 'name':
+			backup_list = backup_movie_names
+			current_list = current_movie_names
+		# If a movie is in the backup photos library and NOT in the current photos library, that means we have accidentally deleted a non-duplicate. Add it to the list.
+		bad_movie_names = []
+		bad_movie_times = []
+		for i in range(len(backup_movie_names)):
+			if backup_list[i] not in current_list:
+				bad_movie_names.append(backup_movie_names[i])
+				bad_movie_times.append(backup_movie_times[i])
 
-	# Print out all non-duplicates accidentally deleted
-	print(str(len(bad_movie_names)) + " non-duplicate(s) accidentally deleted:\n")
-	for i in range(len(bad_movie_names)):
-		print(bad_movie_names[i] + " at " + bad_movie_times[i])
+		# Print out all non-duplicates accidentally deleted, showing movie name and movie time
+		if filter_variant == 'size':
+			print(str(len(bad_movie_names)) + " non-duplicate movie(s) accidentally deleted based on filtering by movie size:\n")
+		else:
+			print(str(len(bad_movie_names)) + " non-duplicate movie(s) accidentally deleted based on filtering by movie name, excluding movies that match by size:\n")
+		print("Format:\n(movie name) from (movie time)\n")
+		for i in range(len(bad_movie_names)):
+			print(bad_movie_names[i] + " from " + bad_movie_times[i])
+		if filter_variant == 'size':
+			print("\n\n\n")
+
+	os.system("rm backup_movie_out.txt")
+	os.system("rm current_movie_out.txt")
--- show_duplicates.py
+++ show_duplicates.py
@@ -1,41 +1,84 @@
 import os
+import sys
+import time
 if __name__ == "__main__":
 
 	# Grab all movies in the current photos library
-	os.chdir("/Users/Aaron/Pictures/Photos Library.photoslibrary")
-	os.system("find . -name '*.avi' > /Users/Aaron/Programming/apple-photo-project/current_movie_out.txt")
-	os.system("find . -name '*.AVI' >> /Users/Aaron/Programming/apple-photo-project/current_movie_out.txt")
-	os.system("find . -name '*.mov' >> /Users/Aaron/Programming/apple-photo-project/current_movie_out.txt")
-	os.system("find . -name '*.MOV' >> /Users/Aaron/Programming/apple-photo-project/current_movie_out.txt")
-	os.system("find . -name '*.mpg' >> /Users/Aaron/Programming/apple-photo-project/current_movie_out.txt")
-	os.chdir("/Users/Aaron/Programming/apple-photo-project")
+	PHOTO_LIB = "/Users/Aaron/Pictures/Photos\ Library.photoslibrary"
+	os.system("find {} -name '*.avi' > current_movie_out.txt".format(PHOTO_LIB))
+	os.system("find {} -name '*.AVI' >> current_movie_out.txt".format(PHOTO_LIB))
+	os.system("find {} -name '*.mov' >> current_movie_out.txt".format(PHOTO_LIB))
+	os.system("find {} -name '*.MOV' >> current_movie_out.txt".format(PHOTO_LIB))
+	os.system("find {} -name '*.mpg' >> current_movie_out.txt".format(PHOTO_LIB))
 	with open("current_movie_out.txt", "r") as f:
 		current_movies = f.readlines()
-	print(str(len(current_movies)) + " total current movies")
+	print(str(len(current_movies)) + " total movie(s) in the photo library\n")
+
+	# Iterate through all movies and get the name, time, and size of each movie
 	movie_names = []
 	movie_times = []
+	movie_sizes = []
 	for i in range(len(current_movies)):
-		movie = current_movies[i][current_movies[i].rfind("/", 0, current_movies[i].rfind("/"))+1:len(current_movies[i])-1]
-		movie_names.append(movie[movie.find("/")+1:])
-		movie_times.append(movie[0:movie.find("/")])
+		movie = current_movies[i][:len(current_movies[i])-1]
+		movie_cut = movie[movie.rfind("/", 0, movie.rfind("/"))+1:len(movie)]
+		movie_names.append(movie_cut[movie_cut.find("/")+1:])
+		movie_times.append(movie_cut[0:movie_cut.find("/")])
+		movie_sizes.append(os.path.getsize(movie))
 
-	movie_set = []
-	duplicate_movie_names = []
-	duplicate_movie_times = []
-	for i in range(len(movie_names)):
+	# First, we want to check flag videos as duplicates if they are the same size. 
+	# Then, we want to flag videos as duplicates if they are the same name.
+	filter_variants = ['size', 'name']
+	for filter_variant in filter_variants:
+		movie_names_set = []
+		movie_times_set = []
+		movie_sizes_set = []
+		duplicate_movie_names = []
+		duplicate_movie_times = []
+		original_movie_names = []
+		original_movie_times = []
+		if filter_variant == 'size':
+			list_to_check = movie_sizes
+			set_to_check = movie_sizes_set
+		elif filter_variant == 'name':
+			list_to_check = movie_names
+			set_to_check = movie_names_set
 
-		# New movies go into the movie set
-		if movie_names[i] not in movie_set:
-			movie_set.append(movie_names[i])
+		# Iterate through all of the movies again
+		for i in range(len(movie_sizes)):
+			# New movies go into the movie set
+			if list_to_check[i] not in set_to_check:
+				movie_names_set.append(movie_names[i])
+				movie_times_set.append(movie_times[i])
+				movie_sizes_set.append(movie_sizes[i])
 
-		# Movies that have already been found are duplicates. Add them to a different list.
+			# Movies that have already been found are duplicates.
+			else:
+				# Get the index of the original movie in the set 
+				original_index = set_to_check.index(list_to_check[i])
+				# If we are filtering movies based on name,
+				# then if the two movies with the matching name have the same size,
+				# then ignore as we have already previously filtered by size 
+				if filter_variant == "name":
+					if movie_sizes[i] == movie_sizes_set[original_index]:
+						continue
+				# Add the duplicate movie to the duplicate lists
+				duplicate_movie_names.append(movie_names[i])
+				duplicate_movie_times.append(movie_times[i])
+				# Add the original movie to the original list
+				original_movie_names.append(movie_names_set[original_index])
+				original_movie_times.append(movie_times_set[original_index])
+
+		# Print out all duplicate movies and show their original counterparts
+		if filter_variant == 'size':
+			print(str(len(duplicate_movie_names)) + " duplicate movie(s) based on filtering by movie size.\n")
 		else:
-			duplicate_movie_names.append(movie_names[i])
-			duplicate_movie_times.append(movie_times[i])
+			print(str(len(duplicate_movie_names)) + " duplicate movie(s) based on filtering by movie name, excluding movies that match by size.\n")
 
-	# Print out all duplicate movies
-	print(str(len(duplicate_movie_names)) + " duplicate movies have been found:\n")
-	for i in range(len(duplicate_movie_names)):
-		print(duplicate_movie_names[i] + " at " + duplicate_movie_times[i])
+		print("Format:\n\n(duplicate movie name) from (duplicate movie time) -> (original movie name) from (original movie time)\n")
+
+		for i in range(len(duplicate_movie_names)):
+			print(duplicate_movie_names[i] + " from " + duplicate_movie_times[i] + " -> " + original_movie_names[i] + " from " + original_movie_times[i])
+		if filter_variant == "size":
+			print("\n\n\n")
 
 	os.system("rm current_movie_out.txt")
